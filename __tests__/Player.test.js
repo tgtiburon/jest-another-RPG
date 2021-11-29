d37d9fb03379f4483c5085f00b6f0ef62af121e0
tgtiburon <tg.tiburon@gmail.com>
2021-11-29 17:28:35 -0500

    added prototypes to player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,3 @@
-
-const { expect } = require('@jest/globals');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
@@ -25,4 +23,27 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
             expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+
+
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    // set inventory to empty below so we can test it.
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
